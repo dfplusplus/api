386ff66078aed5081fc6b15707e0abfd5185b085
Daman56100 <samcraft792@gmail.com>
2020-07-22 11:37:37 +0100

    Added getRecommendedVersion Function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const getRecommendedVersion = functions.https.onRequest(async (request, response) => {
+    admin.initializeApp();
+    let recommendedSnapshot:string = (await admin.database().ref("recommended").once('value')).val();
+    response.send(recommendedSnapshot.replace(/-/g,"."));
+});
